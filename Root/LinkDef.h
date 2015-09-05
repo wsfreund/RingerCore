7ec8c6044092299979a98797b7035b3782d67870
joaoVictorPinto <joaoVictorPinto@github.com>
2015-09-05 09:55:11 +0200

    update analysis tools

---
+++ Root/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+
+#include "RingerCore/MsgStream.h"
+#include "RingerCore/StoreGate.h"
+#pragma link C++ class MsgService+;
+#pragma link C++ class IMsgService+;
+
+
+#endif
