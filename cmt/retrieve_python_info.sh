dee39837110a188ea82990b85baec8b1b71ba130
wsfreund <wsfreund@cern.ch>
2015-12-15 07:18:35 +0100

    Separated python information to a script so that it can be used by other packages.

---
+++ cmt/retrieve_python_info.sh
@@ -0,0 +1,41 @@
+
+arch=`root-config --arch`
+if test "$arch" = "macosx64"
+then
+  include_marker="-I"
+  include_system_marker="-isystem"
+else
+  include_marker="-I"
+  include_system_marker="-isystem"
+fi
+
+PYTHON_EXEC_PATH=`which python`
+PYTHON_EXEC_PATH=`readlink -f $PYTHON_EXEC_PATH`
+PYTHON_INCLUDE_CANDIDATES=${PYTHON_EXEC_PATH//bin\/python*/include\/}
+PYTHON_INCLUDE_CANDIDATES=`find $PYTHON_INCLUDE_CANDIDATES -name "python?.?" -type d` # pick only last result
+vNUM=0
+for path in $PYTHON_INCLUDE_CANDIDATES
+do
+  version=`basename $path`
+  candidateVNUM=${version//python/}
+  candidateVNUM=${candidateVNUM//./}
+  if test "$candidateVNUM" -ge "$vNUM"
+  then
+    PYTHON_LIB_VERSION=$version
+    vNUM=$candidateVNUM
+  fi
+done
+PYTHON_INCLUDE_PATH=""
+for path in $PYTHON_INCLUDE_CANDIDATES
+do
+  if test "`basename $path`" = $PYTHON_LIB_VERSION
+  then
+    if test -e $path/import.h -o -e $path/pyconfig.h
+    then
+      PYTHON_INCLUDE_PATH="$PYTHON_INCLUDE_PATH $include_marker$path"
+    fi
+  fi
+done
+
+export PYTHON_INCLUDE_PATH
+export PYTHON_EXEC_PATH
