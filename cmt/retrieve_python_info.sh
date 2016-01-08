8ec6972cfecbdc79b4aaf848cf9cf3f6c36f87d4
wsfreund <wsfreund@cern.ch>
2016-01-08 20:20:49 +0100

    Fixed resetting "path" variable and

--- cmt/retrieve_python_info.sh
+++ cmt/retrieve_python_info.sh
@@ -9,14 +9,14 @@ else
   include_system_marker="-isystem"
 fi
 
-PYTHON_EXEC_PATH=`which python`
+PYTHON_EXEC_PATH=`pyenv whence --path python 2>/dev/null || which python`
 PYTHON_EXEC_PATH=`readlink -f $PYTHON_EXEC_PATH`
 PYTHON_INCLUDE_CANDIDATES=${PYTHON_EXEC_PATH//bin\/python*/include\/}
 PYTHON_INCLUDE_CANDIDATES=`find $PYTHON_INCLUDE_CANDIDATES -name "python?.?" -type d` # pick only last result
 PYTHON_VERSION_NUM=0
-for candidate in $PYTHON_INCLUDE_CANDIDATES
+for path in $PYTHON_INCLUDE_CANDIDATES
 do
-  version=`basename $candidate`
+  version=`basename $path`
   candidateVNUM=${version//python/}
   candidateVNUM=${candidateVNUM//./}
   if test "$candidateVNUM" -ge "$PYTHON_VERSION_NUM"
@@ -26,13 +26,13 @@ do
   fi
 done
 PYTHON_INCLUDE_PATH=""
-for candidate in $PYTHON_INCLUDE_CANDIDATES
+for path in $PYTHON_INCLUDE_CANDIDATES
 do
-  if test "`basename $candidate`" = $PYTHON_LIB_VERSION
+  if test "`basename $path`" = $PYTHON_LIB_VERSION
   then
-    if test -e $candidate/import.h -o -e $candidate/pyconfig.h
+    if test -e $path/import.h -o -e $path/pyconfig.h
     then
-      PYTHON_INCLUDE_PATH="$PYTHON_INCLUDE_PATH $include_marker$candidate"
+      PYTHON_INCLUDE_PATH="$PYTHON_INCLUDE_PATH $include_marker$path"
     fi
   fi
 done
