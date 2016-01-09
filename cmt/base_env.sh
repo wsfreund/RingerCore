22c0b12af0afcbc9e2820281c77048f5e7c1c461
wsfreund <wsfreund@cern.ch>
2016-01-09 22:07:40 +0100

    Removed boost dependency. Added global base_env.

---
+++ cmt/base_env.sh
@@ -0,0 +1,18 @@
+MAKEFILE=Makefile.RootCore
+
+NEW_ENV_FILE=$PWD/new_env_file.sh
+
+echo -n > $NEW_ENV_FILE
+chmod +x $NEW_ENV_FILE
+
+arch=`root-config --arch`
+if test "$arch" = "macosx64"
+then
+  include_marker="-I"
+  include_system_marker="-isystem"
+else
+  include_marker="-I"
+  include_system_marker="-isystem"
+fi
+
+source $ROOTCOREBIN/../RingerCore/cmt/common_shell_fcns.sh
--- cmt/retrieve_python_info.sh
+++ cmt/retrieve_python_info.sh
@@ -37,6 +37,24 @@ do
   fi
 done
 
+PYTHON_NUMPY_PATH=$(python -c "import numpy; path=numpy.__file__; print path[:path.find('numpy')]") 2> /dev/null
+
+# Add numpy to python path and to include path if we are using afs:
+if test "x$PYTHON_NUMPY_PATH" = "x" -a -e /afs/cern.ch/sw/lcg/external/pyanalysis/ 
+then
+  PYTHON_NUMPY_PATH=`find /afs/cern.ch/sw/lcg/external/pyanalysis/ -maxdepth 1 -name "*$PYTHON_LIB_VERSION" | tail -1`
+  PYTHON_NUMPY_PATH=$PYTHON_NUMPY_PATH/$rootCmtConfig/lib/$PYTHON_LIB_VERSION/site-packages/
+  INCLUDE_NUMPY="$include_system_marker$PYTHON_NUMPY_PATH/numpy/core/include"
+else
+  if test -e $PYTHON_NUMPY_PATH/numpy/core/include; then
+    INCLUDE_NUMPY="$include_system_marker$PYTHON_NUMPY_PATH/numpy/core/include"
+  else
+    if test -e /usr/include/numpy; then
+      INCLUDE_NUMPY="$include_system_marker/usr/include/numpy"
+    fi
+  fi
+fi
+
 export PYTHON_LIB_VERSION
 export PYTHON_VERSION_NUM
 export PYTHON_INCLUDE_PATH
