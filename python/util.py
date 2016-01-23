0d2a9cb3b2319509df76aed0fd4e44c171242e11
wsfreund <wsfreund@cern.ch>
2016-01-23 19:47:25 +0100

    Improved traverse and added class for dealing with numpy data.

--- python/util.py
+++ python/util.py
@@ -288,12 +288,96 @@ class Roc:
     self.fa       = self.faVec[ np.argmax(self.spVec) ]
     self.cut      = self.cutVec[ np.argmax(self.spVec)]
 
+class SetDepth(Exception):
+  def __init__(self, value):
+    self.depth = value
 
-def traverse(o, tree_types=(list, tuple)):
-  "Loop over each holden element"
+def traverse(o, tree_types=(list, tuple),                                                                                                                           
+    max_depth_dist=0, max_depth=np.iinfo(np.uint64).max, 
+    level=0, idx=0, parent=None):
+  """
+  Loop over each holden element. 
+  Can also be used to change the holden values, e.g.:
+
+  a = [[[1,2,3],[2,3],[3,4,5,6]],[[[4,7],[]],[6]],7]
+  for obj, idx, parent in traverse(a): parent[idx] = 3
+  [[[3, 3, 3], [3, 3], [3, 3, 3, 3]], [[[3, 3], []], [3]], 3]
+
+  Examples printing using max_depth_dist:
+
+  In [0]: for obj in traverse(a,(list, tuple),0): print obj
+  (1, 0, [1, 2, 3], 0, 3)
+  (2, 1, [1, 2, 3], 0, 3)
+  (3, 2, [1, 2, 3], 0, 3)
+  (2, 0, [2, 3], 0, 3)
+  (3, 1, [2, 3], 0, 3)
+  (3, 0, [3, 4, 5, 6], 0, 3)
+  (4, 1, [3, 4, 5, 6], 0, 3)
+  (5, 2, [3, 4, 5, 6], 0, 3)
+  (6, 3, [3, 4, 5, 6], 0, 3)
+  (4, 0, [4, 7], 0, 4)
+  (7, 1, [4, 7], 0, 4)
+  (6, 0, [6], 0, 3)
+  (7, 2, [[[1, 2, 3], [2, 3], [3, 4, 5, 6]], [[[4, 7], []], [6]], 7], 0, 1) 
+
+  In [1]: for obj in traverse(a,(list, tuple),1): print obj
+  ([1, 2, 3], 0, [[1, 2, 3], [2, 3], [3, 4, 5, 6]], 1, 3)
+  ([2, 3], 0, [[1, 2, 3], [2, 3], [3, 4, 5, 6]], 1, 3)
+  ([3, 4, 5, 6], 0, [[1, 2, 3], [2, 3], [3, 4, 5, 6]], 1, 3)
+  ([4, 7], 0, [[4, 7], []], 1, 4)
+  ([6], 0, [[[4, 7], []], [6]], 1, 3)
+  ([[[1, 2, 3], [2, 3], [3, 4, 5, 6]], [[[4, 7], []], [6]], 7], 2, None, 1, 1)
+
+  In [2]: for obj in traverse(a,(list, tuple),2): print obj
+  ([[1, 2, 3], [2, 3], [3, 4, 5, 6]], 0, [[[1, 2, 3], [2, 3], [3, 4, 5, 6]], [[[4, 7], []], [6]], 7], 2, 2)
+  ([[4, 7], []], 0, [[[4, 7], []], [6]], 2, 3)
+  ([[[4, 7], []], [6]], 1, [[[1, 2, 3], [2, 3], [3, 4, 5, 6]], [[[4, 7], []], [6]], 7], 2, 2)
+
+  In [3]: for obj in traverse(a,(list, tuple),3): print obj
+  ([[[1, 2, 3], [2, 3], [3, 4, 5, 6]], [[[4, 7], []], [6]], 7], 0, None, 3, 1)
+
+  In [4]: for obj in traverse(a,(list, tuple),4): print obj
+  ([[[1, 2, 3], [2, 3], [3, 4, 5, 6]], [[[4, 7], []], [6]], 7], 1, None, 4, 1)
+
+  In [5]: for obj in traverse(a,(list, tuple),5): print obj
+  <NO OUTPUT>
+
+  """
   if isinstance(o, tree_types):
-    for value in o:
-      for subvalue in traverse(value, tree_types):
-        yield subvalue
-  else:
-    yield o
+    level += 1
+    # FIXME Still need to test max_depth
+    if level > max_depth:
+      yield o, idx, parent, depth_dist
+    skipped = False
+    for idx, value in enumerate(o):
+      try:
+        for subvalue, subidx, subparent, subdepth_dist, sublevel in traverse(value, tree_types, max_depth_dist, max_depth, level, idx, o ):
+          if subdepth_dist == max_depth_dist:
+            if skipped:
+              subdepth_dist += 1
+              break
+            else:
+              yield subvalue, subidx, subparent, subdepth_dist, sublevel 
+          else:
+            subdepth_dist += 1
+            break
+        else: 
+          continue
+      except SetDepth, e:
+        yield o, idx, parent, e.depth, level
+        break
+      if subdepth_dist == max_depth_dist:
+        if skipped:
+          subdepth_dist += 1
+          break
+        else:
+          yield o, idx, parent, subdepth_dist, level
+          break
+      else:
+        if level > (max_depth_dist - subdepth_dist):
+          raise SetDepth(subdepth_dist+1)
+  else:   
+    yield o, idx, parent, 0, level
+
+def setDefaultKey( d, key, val):
+  if not key in d: d[key] = val
