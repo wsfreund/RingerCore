dc72c161b632e27d985dd79eae2dcaafa3f2e974
wsfreund <wsfreund@cern.ch>
2016-03-20 18:07:00 +0100

    Improved tgz loading speed.

--- python/FileIO.py
+++ python/FileIO.py
@@ -2,6 +2,7 @@ import numpy as np
 import cPickle
 import gzip
 import tarfile
+import tempfile
 import os
 import StringIO
 
@@ -46,7 +47,7 @@ def save(o, filename, **kw):
     f.close()
   return filename
 
-def load(filename, decompress = 'auto'):
+def load(filename, decompress = 'auto', allowTmpFile = True):
   """
     Loads an object from disk
   """
@@ -69,14 +70,23 @@ def load(filename, decompress = 'auto'):
       f = tarfile.open(filename, 'r:gz')
       o = []
       for entry in f.getmembers():
-        fileobj = f.extractfile(entry)
-        if entry.name.endswith( '.gz' ) or entry.name.endswith( '.gzip' ):
-          fio = StringIO.StringIO(fileobj.read())
-          fzip = gzip.GzipFile(fileobj=fio)
-          o.append( cPickle.load(fzip) )
+        if allowTmpFile:
+          tmpFolderPath=tempfile.mkdtemp()
+          f.extractall(path=tmpFolderPath)
+          for member in f.getmembers():
+            with open(os.path.join(tmpFolderPath,member.name)) as f_member:
+              o.append( cPickle.load(f_member) )
+          import shutil
+          shutil.rmtree(tmpFolderPath)
         else:
-          o.append( cPickle.load(fileobj) )
-      f.close()
+          fileobj = f.extractfile(entry)
+          if entry.name.endswith( '.gz' ) or entry.name.endswith( '.gzip' ):
+            fio = StringIO.StringIO(fileobj.read())
+            fzip = gzip.GzipFile(fileobj=fio)
+            o.append( cPickle.load(fzip) )
+          else:
+            o.append( cPickle.load(fileobj) )
+        f.close()
       if len(o) == 1:
         return o[0]
       else:
@@ -87,24 +97,39 @@ def load(filename, decompress = 'auto'):
     f.close()
     return o
  
-
-def expandFolders( pathList ):
+def expandFolders( pathList, filters = None):
   """
-    Retrieve all files on the folders on pathList
+    Expand all files using the filters on pathList
   """
   # FIXME debug this the ensure correctness when using multiplie folders
-  if not isinstance( pathList, list ):
+  if not isinstance( pathList, (list,tuple,) ):
     pathList = [pathList]
-  retList = []
-  
+  from glob import glob
+  if filters is None:
+    filters = ['*']
+  if not( type( filters ) in (list,tuple,) ):
+    filters = [ filters ]
+  retList = [[] for idx in range(len(filters))]
   for path in pathList:
-    if not os.path.exists( os.path.expandvars( path ) ):
+    path = os.path.abspath( os.path.expandvars( path ) )
+    if not os.path.exists( path ):
       raise ValueError("Cannot reach path %s" % path )
     if os.path.isdir(path):
-      localList = [ os.path.join(path,f) for f in os.listdir(path) \
-        if os.path.isfile(os.path.join(path,f)) ]
-      retList.extend( expandFolders( localList ) )
+      for idx, filt in enumerate(filters):
+        cList = [ f for f in glob( os.path.join(path,filt) ) ]
+        if cList: 
+          retList[idx].extend(cList)
+      folders = [ os.path.join(path,f) for f in os.listdir( path ) if os.path.isdir( os.path.join(path,f) ) ]
+      if folders:
+        recList = expandFolders( folders, filters )
+        if len(filters) is 1:
+          recList = [recList]
+        for idx, l in enumerate(recList):
+          retList[idx].extend(l)
     else:
-      retList.append( path )
+      for idx, filt in enumerate(filters):
+        if path in glob( os.path.join( os.path.dirname( path ) , filt ) ):
+          retList[idx].append( path )
+  if len(filters) is 1:
+    retList = retList[0]
   return retList
-
