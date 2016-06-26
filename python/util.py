995bcaae3b12d85ccfb5e7d702fa5fd057c43625
Werner Freund <wsfreund@gmail.com>
2016-06-26 20:55:02 -0300

    Added method for checking if LimitedTypeList is in correct shape.

--- python/util.py
+++ python/util.py
@@ -14,7 +14,7 @@ import gzip
 import inspect
 import numpy as np
 
-class NotSetType(type):
+class NotSetType( type ):
   def __bool__(self):
     return False
   __nonzero__ = __bool__
@@ -26,7 +26,7 @@ class NotSetType(type):
 class NotSet( object ): 
   __metaclass__ = NotSetType
 
-class Holder(object):
+class Holder( object ):
   def __init__(self, obj):
     self.obj = obj
   def __call__(self):
