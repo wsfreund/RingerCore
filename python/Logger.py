dd43cd83ac56ad04ca4044c512a35affba4a8ffd
wsfreund <wsfreund@cern.ch>
2016-06-28 04:15:03 +0200

    Added progressbar integrated to Logger.

--- python/Logger.py
+++ python/Logger.py
@@ -44,47 +44,55 @@ def fatal(self, message, *args, **kws):
 logging.Logger.verbose = verbose
 logging.Logger.fatal = fatal
 
-def emit_no_nl(self, record):
+class StreamHandler2( logging.StreamHandler ):
   """
-  Emit a record.
+  Just in case we need a bounded method for emiting without newlines.
+  """
 
-  If a formatter is specified, it is used to format the record.
-  The record is then written to the stream with a trailing newline.  If
-  exception information is present, it is formatted using
-  traceback.print_exception and appended to the stream.  If the stream
-  has an 'encoding' attribute, it is used to determine how to do the
-  output to the stream.
-  """
-  try:
-    msg = self.format(record)
-    stream = self.stream
-    fs = "%s"
-    if not _unicode: #if no unicode support...
-      stream.write(fs % msg)
-    else:
-      try:
-        if (isinstance(msg, unicode) and
-          getattr(stream, 'encoding', None)):
-          ufs = u'%s'
-          try:
-            stream.write(ufs % msg)
-          except UnicodeEncodeError:
-            #Printing to terminals sometimes fails. For example,
-            #with an encoding of 'cp1251', the above write will
-            #work if written to a stream opened or wrapped by
-            #the codecs module, but fail when writing to a
-            #terminal even when the codepage is set to cp1251.
-            #An extra encoding step seems to be needed.
-            stream.write((ufs % msg).encode(stream.encoding))
-        else:
-          stream.write(fs % msg)
-      except UnicodeError:
-        stream.write(fs % msg.encode("UTF-8"))
-    self.flush()
-  except (KeyboardInterrupt, SystemExit):
-    raise
-  except:
-    self.handleError(record)
+  def __init__(self, handler):
+    """
+    Copy ctor
+    """
+    logging.StreamHandler.__init__(self)
+    self._name = handler._name
+    self.level = handler.level
+    self.formatter = handler.formatter
+    self.stream = handler.stream
+
+  def emit_no_nl(self, record):
+    """
+    Monkey patching to emit a record without newline.
+    """
+    try:
+      msg = self.format(record)
+      stream = self.stream
+      fs = "%s"
+      if not logging._unicode: #if no unicode support...
+        stream.write(fs % msg)
+      else:
+        try:
+          if (isinstance(msg, unicode) and
+            getattr(stream, 'encoding', None)):
+            ufs = u'%s'
+            try:
+              stream.write(ufs % msg)
+            except UnicodeEncodeError:
+              #Printing to terminals sometimes fails. For example,
+              #with an encoding of 'cp1251', the above write will
+              #work if written to a stream opened or wrapped by
+              #the codecs module, but fail when writing to a
+              #terminal even when the codepage is set to cp1251.
+              #An extra encoding step seems to be needed.
+              stream.write((ufs % msg).encode(stream.encoding))
+          else:
+            stream.write(fs % msg)
+        except UnicodeError:
+          stream.write(fs % msg.encode("UTF-8"))
+      self.flush()
+    except (KeyboardInterrupt, SystemExit):
+      raise
+    except:
+      self.handleError(record)
 
 
 def getFormatter():
