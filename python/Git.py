9b469aaf5f30853aaccfc4b890e800d79c26cbfb
Werner Freund <wsfreund@gmail.com>
2018-02-14 17:50:58 -0200

    Allow git tags to have letters after the tagging numbers.

--- python/Git.py
+++ python/Git.py
@@ -17,7 +17,7 @@ class GitConfiguration( Configure ):
               , name
               , fname
               , tagArgStr = None
-              , tagStr = '.*-?(\d+-)+\d+(-[0-9a-z]+)?$'
+              , tagStr = '.*-?(\d+[a-z]?-)+\d+(-[0-9a-z]+)?$'
               ):
     """
     -> fname: path pointing to the corresponding --git-dir of the module or any
